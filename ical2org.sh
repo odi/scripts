abf8456e5be357cb66fed14a56608faab53b315a
Oliver Dunkl <oliver.dunkl@gmail.com>
2015-07-17 08:40:42 +0200

    My first version of converting vcal to org files

---
+++ ical2org.sh
@@ -0,0 +1,34 @@
+#! /usr/bin/env bash
+
+summary=""
+location=""
+description=""
+starttime=""
+endtime=""
+
+function trim () {
+    echo $1 | cut -d: -f2
+}
+
+while read line; do
+    if [[ "$line" =~ "SUMMARY" ]]; then
+	summary=`echo $line | cut -d: -f2`
+    elif [[ "$line" =~ "LOCATION" ]]; then
+	location=`echo $line | cut -d: -f2`
+    elif [[ "$line" =~ "DESCRIPTION" ]]; then
+	description=$line
+    elif [[ "$line" =~ "DTSTART" ]]; then
+	t=`echo $line | cut -d: -f2 | sed 's/T/ /'`
+	starttime=`date --date "${t::-4} UTC" "+%Y-%m-%d %a %H:%M"`
+    elif [[ "$line" =~ "DTEND" ]]; then
+	t=`echo $line | cut -d: -f2 | sed 's/T/ /'`
+	endtime=`date --date "${t::-4} UTC" "+%Y-%m-%d %a %H:%M"`	
+    fi
+done < /dev/stdin
+
+echo "* ${summary}"
+echo "  <${starttime}>--<${endtime}>"
+echo "  :PROPERTIES:"
+echo "  :LOCATION: $location"
+echo "  :SUMMARY: $summary"
+echo "  :END:"
